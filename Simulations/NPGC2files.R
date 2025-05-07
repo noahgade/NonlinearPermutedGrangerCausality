ccdf02b2e16b3c74644cf31f5a2a3b84a96adc72
Noah Gade <75758810+noahgade@users.noreply.github.com>
2025-05-07 10:07:00 -0400

    Add files via upload

--- Simulations/NPGC2files.R
+++ Simulations/NPGC2files.R
@@ -3,22 +3,18 @@ library(data.table)
 library(stringr)
 
 # MLP Simulations for NPGC
-# For each iteration, change 'seed.row' item from 1 to 25
-# Builds training instances by using the same files with different set seeds
 
-seed.row <- 1
-
-set.seed(651)
-seeds <- matrix(sample(0:10000, 1800 * 25, replace = TRUE), nrow = 25)
+set.seed(1002)
+seeds <- sample(0:10000, 1800, replace = TRUE)
 for(iter in 1:1800) {
   filename <- paste0('NPGC2_', iter, '.R')
   fileConn <- file(filename)
   writeLines(c(paste0('source("GC.R")'),
                paste0('load("data.RData")'),
                paste0('RNGkind("L', "'", 'Ecuyer-CMRG")'),
-               paste0('set.seed(', seeds[seed.row, iter], ')'),
-               paste0('NPGC2_', seed.row, '_', iter, ' <- npgc(dat = data[[', iter, ']]$data, type = 2, hdim = 100, omega = 1, activation = 1, y_select = 1, z_select = 2, x_select = 3, max_lag = 2, m = 199, k = 10, r = 1)'),
-               paste0('save(NPGC2_', seed.row, '_', iter, ', file = "NPGC2_', seed.row, '_', iter, '.RData")'),
+               paste0('set.seed(', seeds[iter], ')'),
+               paste0('NPGC2_', iter, ' <-  npgc(dat = data[[', iter, ']]$data, type = 2, hdim = 100, omega = 1, activation = 1, y_select = 1, z_select = 2, x_select = 3, max_lag = 2, m = 199, k = 10, r = 1)'),
+               paste0('save(NPGC2_', iter, ', file = "NPGC2_', iter, '.RData")'),
                paste0('print("COMPLETE")')),
              fileConn)
   close(fileConn)
