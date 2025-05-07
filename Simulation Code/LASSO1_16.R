d2da26b451d78f981fa1875307edb603c9b6db7b
Noah Gade <75758810+noahgade@users.noreply.github.com>
2025-05-06 21:09:50 -0400

    Add files via upload

---
+++ Simulation Code/LASSO1_16.R
@@ -0,0 +1,9 @@
+source("GC.R")
+load("data.RData")
+numCores <- as.integer(Sys.getenv("SLURM_NTASKS_PER_NODE")) - 1
+options(mc.cores = numCores)
+RNGkind("L'Ecuyer-CMRG")
+set.seed(8527)
+LASSO1_16 <-  mclapply(data[541:576], function(zz) lasso(dat = zz$data, type = 2, hdim = 100, omega = 1, activation = 1, y_select = 1, z_select = 2, x_select = 3, max_lag = 2, k = 10, r = 25, penalty = 1, lambdas = 2^seq(-1, 7, 0.1), chooselambda = TRUE), mc.preschedule = TRUE, mc.cores = getOption("mc.cores", numCores))
+save(LASSO1_16, file = "LASSO1_16.RData")
+print("COMPLETE")
---
+++ Simulation Code/LASSO2_9.R
@@ -0,0 +1,9 @@
+source("GC.R")
+load("data.RData")
+numCores <- as.integer(Sys.getenv("SLURM_NTASKS_PER_NODE")) - 1
+options(mc.cores = numCores)
+RNGkind("L'Ecuyer-CMRG")
+set.seed(6847)
+LASSO2_9 <-  mclapply(data[289:324], function(zz) lasso(dat = zz$data, type = 2, hdim = 100, omega = 1, activation = 1, y_select = 1, z_select = 2, x_select = 3, max_lag = 2, k = 10, r = 25, penalty = 2, lambdas = 2^seq(-1, 7, 0.1), chooselambda = TRUE), mc.preschedule = TRUE, mc.cores = getOption("mc.cores", numCores))
+save(LASSO2_9, file = "LASSO2_9.RData")
+print("COMPLETE")
