9db5dd34f00ee7718a50e9348138dcc8ec3df504
Noah Gade <75758810+noahgade@users.noreply.github.com>
2025-05-10 07:58:24 -0400

    Update NPGC2files.R

--- Simulations/NPGC2files.R
+++ Simulations/NPGC2files.R
@@ -13,7 +13,7 @@ for(iter in 1:1800) {
                paste0('load("data.RData")'),
                paste0('RNGkind("L', "'", 'Ecuyer-CMRG")'),
                paste0('set.seed(', seeds[iter], ')'),
-               paste0('NPGC2_', iter, ' <-  npgc(dat = data[[', iter, ']]$data, type = 2, hdim = 100, omega = 1, activation = 1, y_select = 1, z_select = 2, x_select = 3, max_lag = 2, m = 199, k = 10, r = 1)'),
+               paste0('NPGC2_', iter, ' <-  npgc(dat = data[[', iter, ']]$data, type = 2, hdim = 100, omega = 1, activation = 0, y_select = 1, z_select = 2, x_select = 3, max_lag = 2, m = 199, k = 10, r = 1)'),
                paste0('save(NPGC2_', iter, ', file = "NPGC2_', iter, '.RData")'),
                paste0('print("COMPLETE")')),
              fileConn)
